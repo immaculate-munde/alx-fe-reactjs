ba9ce363a9f7e74676e00229032df2b834e58608
Munde Immaculate <immaculatemunde1@gmail.com>
2025-07-25 23:29:49 +0300

    Update recipe store path in files

--- recipe-sharing-app/src/components/RecipeDetail.jsx
+++ recipe-sharing-app/src/components/RecipeDetail.jsx
@@ -1,12 +1,13 @@
+// src/components/RecipeDetails.jsx
 import { useParams } from "react-router-dom";
-import useRecipeStore from "../components/recipeStore";
+import useRecipeStore from "../store/recipeStore";
 import EditRecipeForm from "./EditRecipeForm";
 import DeleteRecipeButton from "./DeleteRecipeButton";
 
 export default function RecipeDetails() {
   const { id } = useParams();
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((r) => r.id === Number(id))
+    state.recipes.find((r) => r.id === id)
   );
 
   if (!recipe) return <p>Recipe not found.</p>;
@@ -14,7 +15,7 @@ export default function RecipeDetails() {
   return (
     <div>
       <h2>{recipe.title}</h2>
-      <p>{recipe.instructions}</p>
+      <p>{recipe.description}</p>
       <EditRecipeForm recipe={recipe} />
       <DeleteRecipeButton id={recipe.id} />
     </div>
