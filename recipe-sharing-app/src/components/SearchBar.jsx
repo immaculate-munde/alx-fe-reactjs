02aa4045dd1619a548e3d4c37a960b4dfd7fac38
Munde Immaculate <immaculatemunde1@gmail.com>
2025-07-25 23:43:50 +0300

    Create the search bar component

---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+// src/components/SearchBar.jsx
+import React from 'react';
+import useRecipeStore from '../store/recipeStore';
+
+export default function SearchBar() {
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes by name..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="search-input"
+      style={{
+        width: '100%',
+        padding: '10px',
+        fontSize: '1rem',
+        marginBottom: '1rem',
+        borderRadius: '5px',
+        border: '1px solid silver',
+      }}
+    />
+  );
+}
