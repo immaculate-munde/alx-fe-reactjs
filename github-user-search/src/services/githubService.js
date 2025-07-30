5612d1c78d2d59be393d6d22d5f1a6da6884c0ba
Munde Immaculate <immaculatemunde1@gmail.com>
2025-07-30 14:45:49 +0300

    add fetchUsers function to support advanced search using GitHub Search API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,24 @@
-const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-
 import axios from "axios";
 
+const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 const BASE_URL = "https://api.github.com";
 
-// This function will be implemented in Task 1
+// Task 1: Basic user data by username
 export async function fetchUserData(username) {
   const response = await axios.get(`${BASE_URL}/users/${username}`);
   return response.data;
+}
+
+// Task 2: Advanced search with location and repo filters
+export async function fetchUsers({ username, location, minRepos }) {
+  try {
+    let query = `${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
+
+    const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+    return response.data.items;
+  } catch (error) {
+    throw new Error("Failed to fetch users");
+  }
 }
