40982c03d4be68b771ad268f21cef596f0e571f5
Munde Immaculate <immaculatemunde1@gmail.com>
2025-07-30 13:17:36 +0300

    Create placeholder for a function that will fetch user data

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,11 @@
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com";
+
+// This function will be implemented in Task 1
+export async function fetchUserData(username) {
+  const response = await axios.get(`${BASE_URL}/users/${username}`);
+  return response.data;
+}
