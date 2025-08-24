c134e52ea005700a5c618e690c0784b837aab064
Munde Immaculate <immaculatemunde1@gmail.com>
2025-08-24 22:34:07 +0300

    Route controlled and formik registration forms

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
+
+function App() {
+  return (
+    <div className="space-y-8 p-6">
+      <RegistrationForm />
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
